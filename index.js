456476ef82376c2bb6aae80e6123a475dd65d13d
Aidin <aidin.forooghi@gmail.com>
2023-04-08 00:51:29 -0400

    Passed 4 Tests

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 20;
+const num4 = 8;
+const multiply = num1 * num2; // 31 * 2 = 62
+const random = Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100
+const mod = (num3 + num4) % num4; // (12 + 8) % 8 = 4
+const max = Math.max(num2, num3, num4); // 31
